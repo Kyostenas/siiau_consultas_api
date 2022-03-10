cf5c8b5d9b4c5288336dfe471105bb541241e621
Kyostenas <yaelb.ramirez@alumno.udg.mx>
2022-03-10 00:50:37 -0600

    oferta_siiau_service agregado

---
+++ oferta_siiau_service.py
@@ -0,0 +1,17 @@
+from horario_siiau_service import compactar_horario_por_clases, Clase
+from consulta_siiau_service import ClaseOferta, oferta
+from typing import Tuple, List
+
+#TODO agregar oferta service
+
+
+def estructurar_oferta_como_horario(oferta: Tuple[ClaseOferta]):
+    
+    print(oferta)
+
+
+
+
+
+
+    
