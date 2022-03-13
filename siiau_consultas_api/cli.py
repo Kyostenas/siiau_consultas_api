b95306c671ed0bbe9d4a4d46b9d8caee20a5fd42
Kyostenas <yaelb.ramirez@alumno.udg.mx>
2022-03-13 15:40:13 -0600

    funcion generica de menu en cli funciona

--- siiau_consultas_api/cli.py
+++ siiau_consultas_api/cli.py
@@ -16,7 +16,7 @@
 
 
 from typing import List, Tuple
-from utiles import limpiar_pantalla, regresar_cursor_inicio_pantalla, print_actualizable
+from utiles import limpiar_pantalla, regresar_cursor_inicio_pantalla, print_actualizable, tam_consola
 from utiles import es_alguna_instancia, limpiar_secuencias_ANSI
 from servicio_horario_siiau import __obtener_fecha_completa
 from esquemas import Teclas, Opcion
@@ -32,28 +32,35 @@ def advertencia(texto: str, msj: str = 'ADVE'):
     pre = f'{Style.BRIGHT}{Back.YELLOW}{Style.NORMAL}{Fore.BLACK}{msj}{Style.RESET_ALL}'
     return f'{pre} {Style.BRIGHT}{Fore.YELLOW}{texto}{Style.RESET_ALL}', ancho_real
 
+
 def error(texto: str, msj: str = ' ERR'):
     ancho_real = f'{msj} {texto}'.__len__()
     pre = f'{Style.BRIGHT}{Back.RED}{Style.NORMAL}{Fore.BLACK}{msj}{Style.RESET_ALL}'
     return f'{pre} {Style.BRIGHT}{Fore.RED}{texto}{Style.RESET_ALL}', ancho_real
+
 
 def correcto(texto: str, msj: str = 'CORR'):
     ancho_real = f'{msj} {texto}'.__len__()
     pre = f'{Style.BRIGHT}{Back.GREEN}{Style.NORMAL}{Fore.BLACK}{msj}{Style.RESET_ALL}'
     return f'{pre} {Style.BRIGHT}{Fore.GREEN}{texto}{Style.RESET_ALL}', ancho_real
 
+
 def seleccion(texto: str, cursor: str):
     ancho_real = f'{cursor} {texto}'.__len__()
     pre, _ = sub_titulo(cursor)
-    return f'{Style.BRIGHT}{Fore.BLUE}{pre} {texto}{Style.RESET_ALL}', ancho_real
+    seleccionado = f'{Style.BRIGHT}{Fore.BLUE}{texto}{Style.RESET_ALL}'
+    return f'{pre} {seleccionado}', ancho_real
+
 
 def titulo(texto: str, margin: int = 1):
     ancho_real = f'{" "*margin}{texto}{" "*margin}'.__len__()
     return f'{Style.BRIGHT}{Fore.WHITE}{Back.CYAN}{" "*margin}{texto}{" "*margin}{Style.RESET_ALL}', ancho_real
 
+
 def sub_titulo(texto: str):
     ancho_real = texto.__len__()
     return f'{Style.BRIGHT}{Fore.CYAN}{texto}{Style.RESET_ALL}', ancho_real
+
 
 def log(texto: str, trazo: str):
     dentro, _ = seleccion(trazo)
@@ -64,18 +71,12 @@ def log(texto: str, trazo: str):
     return f'{pre} {texto}', ancho_real
 
 
-
-
-if cols_terminal > 200:
-    cols_terminal = 187
-
-CURSOR = '>>'
+CURSOR = '>'
 DELIMITADOR_RECUADRO = '|'
 EXTRA_ESP_CURSOR = len(CURSOR) + 1
 USUARIO_DEFECTO  = '000000000'
 PROGRAMA, LEN_PROG = sub_titulo('SIIAU Consulta')
 MARGEN_RECUADRO_OPC = 10
-
 
 
 def centrar_linea(linea: str, ancho_total: int, ancho_linea: int, relleno: str=' '):
@@ -149,7 +150,9 @@ def centrar_horizontalmente(texto: str, alto_total: int):
     return centrado
 
 
-def __formatear_una_opcion(opcion_texto: str, max_len: int, i_seleccion: int, i_opcion: int) -> str:
+def __formatear_una_opcion(opcion_texto: str, max_len: int, 
+                           i_seleccion: int, i_opcion: int,
+                           cols_terminal) -> str:
     esta_seleccionado = i_seleccion == i_opcion
     if esta_seleccionado:
         opcion_formateada, len_opcion = seleccion(opcion_texto, CURSOR)
@@ -168,7 +171,7 @@ def __formatear_una_opcion(opcion_texto: str, max_len: int, i_seleccion: int, i_
     return linea_recuadro_centrada
 
 
-def __formatear_opciones(opciones: Tuple[Opcion], i_seleccion: int) -> List[str]:
+def __formatear_opciones(opciones: Tuple[Opcion], i_seleccion: int, cols_terminal) -> List[str]:
     max_len_opciones_crudas = max(list(map(lambda opc: len(opc.mensaje), opciones)))
     opciones_con_formato = []
     for i_opcion, opcion_a_formatear in enumerate(opciones):
@@ -177,14 +180,15 @@ def __formatear_opciones(opciones: Tuple[Opcion], i_seleccion: int) -> List[str]
             opcion_a_formatear.mensaje,
             max_len_opciones_crudas,
             i_seleccion,
-            i_opcion
+            i_opcion,
+            cols_terminal
         )
         opciones_con_formato.append(nueva_linea_opc)
     
     return opciones_con_formato
 
 
-def __formatear_encabezados() -> str:
+def __formatear_encabezados(cols_terminal) -> str:
     fecha = datetime.now()
     fecha = f'{fecha.day}-{fecha.month}-{fecha.year}'
     fecha_completa, len_fech_com = sub_titulo(__obtener_fecha_completa(str(fecha), '-'))
@@ -204,8 +208,15 @@ def __formatear_encabezados() -> str:
 
 def menu(titulo_menu: str, opciones: Tuple[Opcion]):
     i_seleccion = 0
-    # limpiar_pantalla()
+    limpiar_pantalla()
+    regresar_cursor_inicio_pantalla()
     while True:
+        cols_terminal, filas_terminal = tam_consola()
+        if cols_terminal > 187:
+            cols_terminal = 187
+        if filas_terminal > 44:
+            filas_terminal = 44
+
         titulo_formateado, len_titulo = titulo(titulo_menu, 2)
         titulo_centrado = centrar_linea(titulo_formateado, cols_terminal, len_titulo)
         # Para hacer bucle de selecccion.
@@ -215,8 +226,8 @@ def menu(titulo_menu: str, opciones: Tuple[Opcion]):
         if i_seleccion < 0:
             i_seleccion = (len(opciones) - 1)
 
-        encabezados = __formatear_encabezados()
-        opciones_formateadas = __formatear_opciones(opciones, i_seleccion)
+        encabezados = __formatear_encabezados(cols_terminal)
+        opciones_formateadas = __formatear_opciones(opciones, i_seleccion, cols_terminal)
         menu_principal = [
             titulo_centrado,
             '',
@@ -224,11 +235,11 @@ def menu(titulo_menu: str, opciones: Tuple[Opcion]):
         ]
         menu_centrado = centrar_horizontalmente('\n'.join(menu_principal), filas_terminal - 1)
 
-        print(__formatear_encabezados())
-        print(menu_centrado)
+        encabezados_formados = __formatear_encabezados(cols_terminal)
+        print(encabezados_formados, menu_centrado)
         input()
         i_seleccion += 1
-        limpiar_pantalla()
+        regresar_cursor_inicio_pantalla()
 
 if __name__ == '__main__':
     titulo_menu = 'MENU PRINCIPAL'
