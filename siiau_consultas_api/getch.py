e899a2d1263a053cab7ab463d836310e37aca1dc
Kyostenas <yaelb.ramirez@alumno.udg.mx>
2022-03-13 23:57:04 -0600

    getch convertido a funcion

--- siiau_consultas_api/getch.py
+++ siiau_consultas_api/getch.py
@@ -1,39 +1,29 @@
 # Código obtenido de https://code.activestate.com/recipes/134892/
 
-class ObtenerChar:
+def getch():
     """
     Obtener un caracter de entrada sin mostrarlo 
     """
-    def __init__(self):
-        try:
-            self.ejecutar = None
-        except ImportError:  # Significa que no existen esos modulos y no se está en Windows
-            self.ejecutar = None
-    
-    def __call__(self):
-        return self.ejecutar()
+    try:
+        ejecutar = getchwindows()
+    except (ImportError or ModuleNotFoundError):  # Significa que no existen esos modulos y no se está en Windows
+        ejecutar = getchunix()
+    return ejecutar
 
 
-class __ObtenerChUnix:
-    def __init__(self):  # Intento de importación
-        import tty, sys
-    
-    def __call__(self):
-        import sys, tty, termios
-        descriptor_de_archivo = sys.stdin.fileno()
-        vieja_config = termios.tcgetattr(descriptor_de_archivo)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(descriptor_de_archivo, termios.TCSADRAIN, vieja_config)
-        return ch
+def getchunix():
+    import tty, sys, termios
+    descriptor_de_archivo = sys.stdin.fileno()
+    vieja_config = termios.tcgetattr(descriptor_de_archivo)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(descriptor_de_archivo, termios.TCSADRAIN, vieja_config)
+
+    return ch
 
 
-class __ObtenerChWindows:
-    def __init__(self):  # Intento de importación
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
+def getchwindows():
+    import msvcrt
+    return msvcrt.getch()
